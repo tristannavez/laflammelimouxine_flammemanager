dd069468b83637dfaaa9742722af7b1e17095b64
tristannavez <trs.nvz@outlook.fr>
2023-01-19 20:43:35 +0100

    Création de filtres personnalisés pour filtrer l'état des livraisons sur tout sauf complet et l'état des chantiers sur tout sauf terminé

--- flammemanager/admin.py
+++ flammemanager/admin.py
@@ -17,17 +17,73 @@ class ClientAdmin(admin.ModelAdmin):
 
 class CommandeAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'client', 'prix_achat_ht', 'prix_vente_ht', 'etat_commande')
-    list_filter = ('client', 'etat_commande')
+    list_filter = ('etat_commande', 'produits')
+    search_fields = ('client__nom',)
+    search_help_text = ("Rechercher un client")
+
+class LivraisonEtatListFilter(admin.SimpleListFilter):
+    title = 'Etat de la livraison'
+    parameter_name = 'état'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('commandé_ou_livré', 'Commandé ou Livré'),
+            ('commandé', 'Commandé'),
+            ('livré', 'Livré'),
+            ('complet', 'Complet'),
+
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'commandé_ou_livré':
+            return queryset.filter(etat_livraison__in=['commandé', 'livré'])
+        if self.value() == 'commandé':
+            return queryset.filter(etat_livraison__in=['commandé'])
+        if self.value() == 'livré':
+            return queryset.filter(etat_livraison__in=['livré'])
+        if self.value() == 'complet':
+            return queryset.filter(etat_livraison__in=['complet'])
+
 
 class LivraisonAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'client', 'etat_livraison', 'commande', 'date_reelle')
-    list_filter = ('client', 'etat_livraison', 'date_reelle')
+    list_filter = (LivraisonEtatListFilter,'client', 'date_reelle')
+    search_fields = ('client__nom',)
+    search_help_text = ("Rechercher un client")
 
+
+class ChantierEtatListFilter(admin.SimpleListFilter):
+    title = 'Etat du chantier'
+    parameter_name = 'état'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('en attente, planifié ou validé', 'En attente, planifié ou validé'),
+            ('en attente', 'En attente'),
+            ('planifié', 'Planifié'),
+            ('validé', 'Validé'),
+            ('terminé', 'Terminé'),
+
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'en attente, planifié ou validé':
+            return queryset.filter(etat_chantier__in=['en attente', 'planifié', 'validé'])
+        if self.value() == 'en attente':
+            return queryset.filter(etat_chantier__in=['en attente'])
+        if self.value() == 'planifié':
+            return queryset.filter(etat_chantier__in=['planifié'])
+        if self.value() == 'validé':
+            return queryset.filter(etat_chantier__in=['validé'])
+        if self.value() == 'terminé':
+            return queryset.filter(etat_chantier__in=['validé'])
 
 class ChantierAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'client', 'etat_chantier', 'nombre_de_jours', 'date_intervention', 'type_chantier')
-    list_filter = ('etat_chantier', 'client', 'nombre_de_jours', 'date_intervention', 'type_chantier')
-
+    list_filter = (ChantierEtatListFilter, 'nombre_de_jours', 'date_intervention', 'type_chantier')
+    search_fields = ('client__nom',)
+    search_help_text = ("Rechercher un client")
+    exclude = ('livraisons',)
 class ProduitAdmin(admin.ModelAdmin):
     list_display = ('nom', 'fournisseur')
     list_filter = ('nom', 'fournisseur')
