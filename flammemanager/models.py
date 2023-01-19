de73dd3d8d63898a504d7e9a8d4852e36ef287b5
tristannavez <trs.nvz@outlook.fr>
2023-01-19 18:47:33 +0100

    Ajouter nombre de jours dans model Chantier

--- flammemanager/models.py
+++ flammemanager/models.py
@@ -37,6 +37,8 @@ class Chantier(models.Model):
     livraisons = models.ManyToManyField(Livraison)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     etat_chantier = models.CharField(max_length=20, choices=[('en attente', 'En attente'), ('planifié', 'Planifié'), ('validé', 'Validé'), ('terminé', 'Terminé')])
+    nombre_de_jours = models.DecimalField(max_digits=3, decimal_places=1)
+
     def __str__(self):
         return 'Chantier pour ' + str(self.client.nom)
 
